71b9c9af24758f270cb33676a17d99437c690c37
jimenarw <84816847+JimenaRW@users.noreply.github.com>
2022-06-09 19:03:13 -0300

    base, entrega 1 (imagen visible al clickear)

--- guess-pokemon/src/App.tsx
+++ guess-pokemon/src/App.tsx
@@ -1,5 +1,44 @@
+import {FormEvent, useEffect, useState} from "react";
+
+import {Pokemon} from "./types";
+import api from "./api";
+
 function App() {
-  return <main>Let&apos;s get this party started</main>;
+  const [pokemon, setPokemon] = useState<Pokemon>();
+
+  useEffect(() => {
+    api.random().then(setPokemon);
+  }, []);
+
+  const handleVisible = (e: FormEvent) => {
+    e.preventDefault();
+
+    let imagePokemon = document.querySelector<HTMLDivElement>(".imagePokemon");
+
+    imagePokemon.style.filter = "none";
+  };
+
+  return (
+    <main>
+      <h1>Quién es este pokemon?</h1>
+      {pokemon ? (
+        <div
+          key={pokemon.id}
+          className={"imagePokemon"}
+          id={"PokemonId"}
+          style={{backgroundImage: `url(${pokemon.image})`}}
+        />
+      ) : (
+        <p>Cargando pokemon...</p>
+      )}
+      <form>
+        <input className="nes-input" type="text" />
+        <button className="nes-btn is-primary" type="submit" onClick={(e) => handleVisible(e)}>
+          Adivinar
+        </button>
+      </form>
+    </main>
+  );
 }
 
 export default App;
