b0599c10afbb47da623e6bb6244f3af22f28a934
jimenarw <84816847+JimenaRW@users.noreply.github.com>
2022-06-13 19:45:55 -0300

    Entrega 3. Persistir historial de juego

--- guess-pokemon/src/App.tsx
+++ guess-pokemon/src/App.tsx
@@ -4,38 +4,63 @@ import swal from "sweetalert";
 import {Pokemon} from "./types";
 import api from "./api";
 
+const initialStateCount = {
+  hits: 0,
+  fails: 0,
+};
+const pokemonStorage = JSON.parse(localStorage.getItem("GuesPokemon") as string);
+
 function App() {
   const [pokemon, setPokemon] = useState<Pokemon>();
   const [replay, setReplay] = useState<boolean>(true);
+  const [count, setCount] = useState(pokemonStorage || initialStateCount);
 
   useEffect(() => {
     api.random().then(setPokemon);
     setReplay(false);
   }, [replay]);
 
-  const handleVisible = async (e: FormEvent) => {
+  useEffect(() => localStorage.setItem("GuesPokemon", JSON.stringify(count)), [count]);
+
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
 
     let pokemonName = document.getElementById("pok-name") as HTMLInputElement | null;
     let imagePokemon = document.querySelector(".imagePokemon") as HTMLDivElement;
 
-    imagePokemon.classList.add("visible");
-
     let inputValue = pokemonName?.value.replace(/[.\s]+/g, "");
 
     if (inputValue?.toLowerCase() === pokemon?.name) {
+      imagePokemon.classList.add("visible");
+
       await swal({
         title: "Good job!",
-        text: `Su nombre es ${pokemon?.name}!`,
+        text: `Su nombre es ${pokemon?.name.toUpperCase()}!`,
         icon: "success",
         className: "nes-container",
+        button: {
+          className: "nes-btn is-primary",
+        },
+      });
+
+      setCount({
+        ...count,
+        hits: ++count.hits,
       });
     } else {
       await swal({
         title: "Try again",
-        text: `Su nombre es ${pokemon?.name}!`,
+        text: `Estuvo cerca! Sigue intentando.`,
         icon: "error",
         className: "nes-container",
+        button: {
+          className: "nes-btn is-primary",
+        },
+      });
+
+      setCount({
+        ...count,
+        fails: ++count.fails,
       });
     }
   };
@@ -43,35 +68,49 @@ function App() {
   const handleReplay = () => {
     let imagePokemon = document.querySelector(".imagePokemon") as HTMLDivElement;
 
-    imagePokemon.classList.remove("visible");
+    imagePokemon.classList.contains("visible") && imagePokemon.classList.remove("visible");
 
     setReplay(true);
   };
 
+  const handleReset = () => setCount(initialStateCount);
+
   return (
     <main>
       <h1>Quién es este pokemon?</h1>
-      {pokemon ? (
-        <>
-          <div
-            key={pokemon.id}
-            className={"imagePokemon"}
-            id={"PokemonId"}
-            style={{backgroundImage: `url(${pokemon.image})`}}
-          />
-          <button className="nes-btn is-primary" type="submit" onClick={handleReplay}>
+      <section>
+        <article>
+          {pokemon ? (
+            <div
+              key={pokemon.id}
+              className={"imagePokemon"}
+              id={"PokemonId"}
+              style={{backgroundImage: `url(${pokemon.image})`}}
+            />
+          ) : (
+            <p>Cargando pokemon...</p>
+          )}
+          <form>
+            <input className="nes-input" id="pok-name" type="text" />
+            <button className="nes-btn is-primary" type="submit" onClick={(e) => handleSubmit(e)}>
+              Adivinar
+            </button>
+          </form>
+          <button className="nes-btn is-success" type="submit" onClick={handleReplay}>
             Replay
           </button>
-        </>
-      ) : (
-        <p>Cargando pokemon...</p>
-      )}
-      <form>
-        <input className="nes-input" id="pok-name" type="text" />
-        <button className="nes-btn is-primary" type="submit" onClick={(e) => handleVisible(e)}>
-          Adivinar
-        </button>
-      </form>
+        </article>
+        <aside>
+          <h1>Contador</h1>
+          <hr />
+          <p>Aciertos: {count.hits}</p>
+          <p>Fallos: {count.fails}</p>
+          <hr />
+          <button className="nes-btn is-warning" onClick={handleReset}>
+            resetear contador
+          </button>
+        </aside>
+      </section>
     </main>
   );
 }
